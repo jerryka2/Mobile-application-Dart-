140d2ffd5d55b1951083b39457c794bc2cb733e6
jerryka2 <121224202+jerryka2@users.noreply.github.com>
2024-11-19 11:16:21 +0545

    Add files via upload

---
+++ mult.dart
@@ -0,0 +1,12 @@
+// Function to multiply two numbers using dart.
+
+int multiplyNumbers(int a, int b) {
+  return a * b;
+}
+
+void main() {
+  int a = 10;
+  int b = 5;
+
+  print("Multiplication: ${multiplyNumbers(a, b)}");
+}
