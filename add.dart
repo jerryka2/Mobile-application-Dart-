d96a5425201a3fce8366b9901f13b05cf35a9cae
jerryka2 <121224202+jerryka2@users.noreply.github.com>
2024-11-19 11:16:05 +0545

    Add files via upload

---
+++ add.dart
@@ -0,0 +1,12 @@
+// Function to add two numbers using dart.
+
+int addNumbers(int a, int b) {
+  return a + b;
+}
+
+void main() {
+  int a = 10;
+  int b = 5;
+
+  print("Addition: ${addNumbers(a, b)}");
+}
