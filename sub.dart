eda2883da4bf842126e4e12fd89e265dbff554cc
jerryka2 <121224202+jerryka2@users.noreply.github.com>
2024-11-19 11:16:40 +0545

    Add files via upload

---
+++ sub.dart
@@ -0,0 +1,12 @@
+// Function to subtract two numbers using dart.
+
+int subtractNumbers(int a, int b) {
+  return a - b;
+}
+
+void main() {
+  int a = 10;
+  int b = 5;
+
+  print("Subtraction: ${subtractNumbers(a, b)}");
+}
